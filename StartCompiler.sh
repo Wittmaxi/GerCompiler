9efe68b1497fcf1c06b24ccd9c00adbc70c404a6
MaximilianJugendForscht <wittmaxi@outlook.de>
2016-12-18 18:40:39 +0100

    Update StartCompiler.sh
    ARCH-Support... ... ... ... ...

--- StartCompiler.sh
+++ StartCompiler.sh
@@ -2,7 +2,10 @@
 
 echo "Installiere die neueste Assembler-Version"
 
-sudo apt install nasm
+sudo apt install nasm #for ubuntu-users
+sudo Pacman -P nasm #Für Dich, R., der Das Testen wird, in der Hoffnung, dass ich mit Ubuntu aufhöre xD
+
+clear
 
 echo "Starte den Compiler. Schliessen sie dieses Fenster nicht, da das fertige Programm hier ausgeführt werden wird."
 
