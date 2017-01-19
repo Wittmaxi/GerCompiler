24c6d72abfefbc25d69fbb5098b6f076e451e83b
MaximilianJugendForscht <wittmaxi@outlook.de>
2017-01-19 19:36:17 +0100

    Delete StartCompiler.sh

--- StartCompiler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Installiere die neueste Assembler-Version"
-
-sudo apt install nasm
-
-echo "Starte den Compiler. Schliessen sie dieses Fenster nicht, da das fertige Programm hier ausgeführt werden wird."
-
-sudo ./Compiler
